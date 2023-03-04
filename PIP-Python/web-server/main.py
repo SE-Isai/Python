000c0f7d66b6429656304f40c9a449b0953ecfdb
Isai Fomperoza <isaifomperoza@gmail.com>
2023-03-03 21:40:31 -0600

    Added web server using FastApi

--- PIP-Python/web-server/main.py
+++ PIP-Python/web-server/main.py
@@ -1,4 +1,16 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return[1,2,3,4,5,6,7,8]
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_list():
+    return "<h1>This is a text to return a website from Python using FastApi</h1>"
 
 def run():
     store.get_categories()
