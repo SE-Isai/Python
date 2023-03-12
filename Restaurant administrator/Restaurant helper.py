48f02e987c72a4e84f19b9bb6237f7a6fb5ec524
IsaiFomperoza <isaifomperoza@gmail.com>
2023-03-12 09:11:58 -0600

    Added cost of the product

--- Restaurant administrator/Restaurant helper.py
+++ Restaurant administrator/Restaurant helper.py
@@ -4,52 +4,64 @@ import tkinter as tk
 # Función para mostrar la lista de productos
 def mostrar_productos():
     productos_listbox.delete(0, tk.END)
-    for nombre, precio in productos.items():
-        productos_listbox.insert(tk.END, f"{nombre}: ${precio:.2f}")
+    for producto in productos:
+            nombre = producto["Nombre"]
+            precio = producto["Precio"]
+            costo = producto["Costo"]
+            productos_listbox.insert(tk.END, f"{nombre}: ${precio:.2f} Costo: {costo:.2f}")
+        
 
 # Función para agregar un producto
 def agregar_producto():
     nombre = nombre_entry.get()
     precio = float(precio_entry.get())
-    productos[nombre] = precio
-    guardar_productos()
+    costo = float(costo_entry.get())
+    producto = {"Nombre": nombre, "Precio": precio, "Costo": costo}
+    productos.append(producto)
+    guardar_productos(productos)
     mostrar_productos()
     nombre_entry.delete(0, tk.END)
     precio_entry.delete(0, tk.END)
+    costo_entry.delete(0, tk.END)
 
 # Función para eliminar un producto
 def eliminar_producto():
     nombre = productos_listbox.get(tk.ACTIVE).split(":")[0]
-    del productos[nombre]
-    guardar_productos()
+    for i, producto in enumerate(productos):
+        if producto["Nombre"] == nombre:
+            del productos[i]
+            break
+    guardar_productos(productos)
     mostrar_productos()
 
-# Función para mostrar el total de costos
-def mostrar_total():
-    total = sum(productos.values())
-    total_label.config(text=f"El total de costos es ${total:.2f}")
+
 
 # Función para leer la lista de productos desde el archivo
 def leer_productos():
-    productos = {}
+    productos = []
     if os.path.exists("productos.txt"):
         with open("productos.txt", "r") as archivo:
             lineas = archivo.readlines()
             for linea in lineas:
-                nombre, precio = linea.strip().split(",")
-                productos[nombre] = float(precio)
+                nombre, precio, costo = linea.strip().split(",")
+                producto = {"Nombre": nombre, "Precio": float(precio), "Costo": float(costo)}
+                productos.append(producto)
     return productos
 
 # Función para guardar la lista de productos en el archivo
-def guardar_productos():
+def guardar_productos(productos):
     with open("productos.txt", "w") as archivo:
-        for nombre, precio in productos.items():
-            archivo.write(f"{nombre},{precio}\n")
+        for producto in productos:
+            nombre = producto["Nombre"]
+            precio = producto["Precio"]
+            costo = producto["Costo"]
+            archivo.write(f"{nombre},{precio},{costo}\n")
+    print('Archivo guardado satisfactoriamente')
 
 # Función principal para ejecutar el programa
 def main():
     # Inicializar la interfaz gráfica
-    global productos_listbox, nombre_entry, precio_entry, total_label, productos
+    global productos_listbox, nombre_entry, precio_entry, total_label, costo_entry, productos
     productos = leer_productos()
     ventana = tk.Tk()
     ventana.title("Sistema de administración de costos de restaurante")
@@ -61,6 +73,8 @@ def main():
     nombre_entry = tk.Entry(ventana)
     precio_label = tk.Label(ventana, text="Precio:")
     precio_entry = tk.Entry(ventana)
+    costo_label = tk.Label(ventana, text="Costo:")
+    costo_entry = tk.Entry(ventana)
     agregar_boton = tk.Button(ventana, text="Agregar", command=agregar_producto)
     eliminar_boton = tk.Button(ventana, text="Eliminar", command=eliminar_producto)
     total_label = tk.Label(ventana, text="")
@@ -72,13 +86,14 @@ def main():
     nombre_entry.grid(row=2, column=1, padx=10, pady=10)
     precio_label.grid(row=3, column=0, padx=10, pady=10)
     precio_entry.grid(row=3, column=1, padx=10, pady=10)
-    agregar_boton.grid(row=4, column=0, padx=10, pady=10)
-    eliminar_boton.grid(row=4, column=1, padx=10, pady=10)
-    total_label.grid(row=5, column=0, padx=10, pady=10)
+    costo_label.grid(row=4, column=0, padx=10, pady=10)
+    costo_entry.grid(row=4, column=1, padx=10, pady=10)
+    agregar_boton.grid(row=5, column=0, padx=10, pady=10)
+    eliminar_boton.grid(row=5, column=1, padx=10, pady=10)
+    total_label.grid(row=6, column=0, padx=10, pady=10)
 
     # Mostrar la lista de productos y el total de costos
     mostrar_productos()
-    mostrar_total()
 
     # Ejecutar la interfaz gráfica
     ventana.mainloop()
